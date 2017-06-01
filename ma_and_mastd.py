4ea2e13d234aac45001edc845f75ed4ddcb4094d
Jasmin <jasmin_bogatinovski@outlook.com>
2017-06-01 12:27:37 -0700

    Update na report

--- ma_and_mastd.py
+++ ma_and_mastd.py
@@ -7,10 +7,13 @@ from reading import read_sm_csv_by_day_sequence
 startTime = time()
 
 frame_size = 480  #rolling horizon of 8 minutes e dovolno golem
-dictionaryData, keys = read_sm_csv_by_day_sequence(1, 6, "01-06-2012", "05-06-2012")
+dictionaryData, keys = read_sm_csv_by_day_sequence(1, 7, "01-08-2012", "01-09-2012")
 
 new = dictionaryData
+k = 62
 for key in keys:
+    figName = ''
+    plt.figure(k)
     X = new[key].pall.values
     t = np.arange(0, X.shape[0])
     D = pd.Series(X, t)
@@ -21,9 +24,14 @@ for key in keys:
 endTime = time()
 print('Vreme na izvrasuvanje na skriptata: ', endTime-startTime)
 
-
 #plt.plot(t, X, c='red')
 #plt.plot(t, data_ma)
 #plt.plot(t,data_ma_std,'green')
 #plt.savefig('Reports\ma_reports\Eight_min_rolling_horizon_standard_deviation.png')
 #plt.show()
+#plt.plot(t, X, c='red')
+#plt.plot(t, data_ma)
+#plt.plot(t, data_ma_std, 'green')
+#figName = 'Reports\ma_reports' + '\Eight_min_rolling_horizon_standard_deviation'+str(k)+'.png'
+#plt.savefig(figName)
+#k=k+1
